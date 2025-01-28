b7927a319e5fcd89d3027dd9d3ab7e62537b4f77
taphite <martinorg25@gmail.com>
2025-01-28 17:54:19 +0200

    Added controls menu

--- lolli-needus/Skriptid/menu.gd
+++ lolli-needus/Skriptid/menu.gd
@@ -11,3 +11,6 @@ func _on_start_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+func _on_controls_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Stseenid/controls.tscn")
